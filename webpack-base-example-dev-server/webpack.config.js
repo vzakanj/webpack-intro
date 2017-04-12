f54d209ca404755c9cc245f5632b02a8df4c9c06
Vedran Zakanj <vedran@mono-software.com>
2017-04-12 13:01:16 +0200

    Add watching to dev server example.

--- webpack-base-example-dev-server/webpack.config.js
+++ webpack-base-example-dev-server/webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
         path: path.resolve("./dist"),
         filename: "bundle.js"
     },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+    },
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         port: 5000
