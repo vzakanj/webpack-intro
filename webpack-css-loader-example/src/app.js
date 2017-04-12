14c48865133588ae1278dbc9a8ca04d1b5ea95f1
Vedran Zakanj <vedran@mono-software.com>
2017-04-12 13:59:28 +0200

    Add webpack-css-loader-example.

---
+++ webpack-css-loader-example/src/app.js
@@ -0,0 +1,5 @@
+import '../styles/app.css';
+import Math from './myMath';
+
+const math = new Math();
+console.log(`Sum calculated using myMath: ${math.sum(3, 10)}`);
---
+++ webpack-css-loader-example/webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+
+module.exports = {
+    context: path.resolve("./"),
+    entry: "./src/app.js",
+    output: {
+        path: path.resolve("./dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 5000
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["latest"]
+                    }
+                }
+            },
+            {
+                test: /\.css/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    }
+};
